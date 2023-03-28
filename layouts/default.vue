23cfa52208e71a59461bbe632394d2d97923d2da
mick <mickheudre@gmail.com>
2023-03-28 16:17:38 +0200

    plus de contenu et ajout d'un calendrier

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,55 @@
 <template>
-    <div class="min-h-screen w-screen bg-back flex flex-col overflow-hidden">
+    <div class="min-h-screen bg-back flex flex-col overflow-hidden">
         <div class="flex items-center  sticky top-4 px-4 sm:px-8">
             <div class="flex gap-8 lg:gap-24 items-center">
                 <NuxtLink to="/"><Logo class="h-12 w-12 flex-none sm:w-16 sm:h-16"/></NuxtLink>
-                <a class="hidden  sm:flex flex-none font-sans uppercase font-semibold">Qui suis-je ?</a>
-                <a class="hidden sm:flex flex-none font-sans uppercase font-semibold">Mes services</a>
+                <a class="hidden  sm:flex flex-none font-sans uppercase font-semibold" href="http://www.mickheudre.me">Qui suis-je ?</a>
+                <a class="hidden sm:flex flex-none font-sans uppercase font-semibold" href="/">Mes services</a>
                 <a class="hidden sm:flex flex-none font-sans uppercase font-semibold">Me contacter</a>
             </div>
-            <div class="marquee  bg-main text-back flex transform rotate-45 translate-x-1/3  px-8 py-2">
-                    <div class="marquee__inner" aria-hidden="true">
-                        <p  class=" w-max" v-for="item in 12">
-                            Open to Work <span class="px-2">-</span>
-                        </p>
-                    </div>
-                    </div>
-            </div>                
-            <slot />
+            <div class="marquee  bg-main text-back flex transform rotate-45 translate-x-1/3 xl:translate-x-1/2 px-8 py-2">
+                <div class="marquee__inner" aria-hidden="true">
+                    <p  class=" w-max" v-for="item in 12">
+                        Open to Work <span class="px-2">-</span>
+                    </p>
+                </div>
+            </div>
         </div>
-    </template>
-    
-    
-    <style>
-    .marquee {
-        position: relative;
-        overflow: hidden;
-        --offset: 20vw;
-        --move-initial: calc(-25% + var(--offset));
-        --move-final: calc(-50% + var(--offset));
-    }
-    
-    .marquee__inner {
-        width: fit-content;
-        display: flex;
-        position: relative;
+        <div class="flex justify-center w-full">
+            <slot />
+            
+        </div>                
+    </div>
+</template>
+
+
+<style>
+.marquee {
+    position: relative;
+    overflow: hidden;
+    --offset: 20vw;
+    --move-initial: calc(-25% + var(--offset));
+    --move-final: calc(-50% + var(--offset));
+}
+
+.marquee__inner {
+    width: fit-content;
+    display: flex;
+    position: relative;
+    transform: translate3d(var(--move-initial), 0, 0);
+    animation: marquee 5s linear infinite;
+}
+
+
+@keyframes marquee {
+    0% {
         transform: translate3d(var(--move-initial), 0, 0);
-        animation: marquee 5s linear infinite;
     }
     
-    
-    @keyframes marquee {
-        0% {
-            transform: translate3d(var(--move-initial), 0, 0);
-        }
-        
-        100% {
-            transform: translate3d(var(--move-final), 0, 0);
-        }
+    100% {
+        transform: translate3d(var(--move-final), 0, 0);
     }
-    
-    
+}
+
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,76 @@
 <template>
-  <div class="flex flex-col px-4 sm:px-8 lg:px-32 pt-4">
+  <div class="flex flex-col px-4 sm:px-8 lg:px-32 w-full pt-4 xl:pt-14 max-w-screen-2xl ">
     <div class="w-full my-8 sm:my-0">
-      <div class="uppercase text-3xl md:text-6xl lg:text-8xl italic font-semibold border-b-4 md:border-b-8 w-full border-main text-main my-4 md:my-8 ">
+      <div class="uppercase text-3xl md:text-6xl lg:text-8xl font-semibold border-b-4 md:border-b-8 w-full border-main text-main my-4 md:my-8 ">
         Votre Application
       </div>
-      <div class="uppercase text-3xl md:text-6xl italic lg:text-8xl font-semibold border-b-4 md:border-b-8 w-full border-main text-main text-right md:my-8 ">
+      <div class="uppercase text-3xl md:text-6xl lg:text-8xl font-semibold border-b-4 md:border-b-8 w-full border-main text-main text-right md:my-8 ">
         Clés en main
       </div>
     </div>
     <div class="flex flex-col items-center  md:items-stretch md:flex-row w-full h-full  my-4">
-      <img src="~/assets/images/index.jpg" class="w-48 md:w-96 "/>
-      <div class="flex flex-col items-center md:items-end justify-start gap-y-4 w-full"> 
-        <div class="flex flex-wrap justify-between md:justify-end w-full md:gap-x-8  my-4">
+      <div class="flex justify-center sm:w-1/2">
+        <img src="~/assets/images/index.jpg" class="w-48 md:w-96 2xl:w-5/6"/>
+      </div>
+      <div class="flex flex-col items-center md:items-end justify-start gap-y-4 sm:w-1/2"> 
+        <div class="flex flex-wrap justify-between md:justify-end w-full md:gap-x-8  my-4 2xl:my-16" >
           <span class=" font-serif text-xl md:text-2xl text-main">Design UI/UX <Star class="inline pb-1"/></span>
           <span class=" font-serif text-xl md:text-2xl text-main">Développement <Star class="inline pb-1"/></span>
           <span class=" font-serif text-xl md:text-2xl text-main">Conception Agile & Lean <Star class="inline pb-1"/></span>
         </div>
         <div class="sm:w-4/5 lg:w-2/3">
-          <h4 class="font-semibold text-xl my-2 sm:text-right">
-            Construisons ensemble l’application qu’il vous faut
+          <h4 class="font-semibold text-xl lg:text-2xl my-2 sm:text-right">
+            Construisons ensemble l’application qu’il vous faut, <span class="text-lg font-normal">de l’émergence de l’idée à la mise en production, conçue pour répondre à vos besoins et ceux des utilisateurs</span>
           </h4>
-          <h5 class="sm:text-right">De l’émergence de l’idée à la mise en production, conçue pour répondre à vos besoins et ceux des utilisateurs</h5>
+          
           
         </div>
-        <button class="font-bold px-12 lg:px-16 py-5 bg-main text-back rounded-md font-sans uppercase my-8">Parlons de vos projets</button>
-
+        <button class="font-bold px-12 lg:px-16 py-5 bg-main text-back rounded-md font-sans uppercase my-8"><a href="#booking">Parlons de vos projets</a></button>
+        
       </div>
     </div>
-
-    <div class="flex flex-col sm:flex-row my-16 gap-16 items-center justify-between">
-      <div class="flex flex-col  gap-8 sm:w-1/2">
-        <h2 class=" font-semibold text-4xl lg:text-8xl font-serif">Une offre sur mesure</h2>
-
+    
+     <div class="flex flex-col sm:flex-row my-16  items-center justify-between">
+      <div class="flex flex-col  gap-16 sm:w-1/2">
+        <h2 class=" font-semibold text-4xl lg:text-7xl font-serif">Une offre sur mesure</h2>
+        
         <div class="flex gap-8 w-full">
           <div class="bg-main h-24  rounded-sm flex">
             <span class="text-back text-4xl font-bold ml-8 mr-2 mt-2 text-right">1</span>
           </div>
-          <div>
+          <div class="w-full">
             <h4 class="text-2xl font-semibold mb-4">Vous voulez tester une idée</h4>
-            <p>Vous voulez tester une idée et cherchez quelqu’un pour développer rapidement une application afin de recueillir des retours utilisateurs au plus vite.</p>
-            <p>Vous avez des contraintes de temps ou de budget</p>
+            <p>Vous voulez tester une idée et cherchez quelqu’un pour développer rapidement une application afin de recueillir des retours utilisateurs.</p>
+            <p>Faisons de vos contraintes de temps et de budget une force pour se concentrer sur l'essentiel. </p>
           </div>
         </div>
-      </div>
-      <img class="w-48 md:w-96" src="../assets/images/offer.jpg"/>
+        <div class="flex gap-8 w-full">
+          <div class="bg-main h-24  rounded-sm flex">
+            <span class="text-back text-4xl font-bold ml-8 mr-2 mt-2 text-right">2</span>
+          </div>
+          <div class="w-full">
+            <h4 class="text-2xl font-semibold mb-4">Vous cherchez à améliorer un produit existant</h4>
+            <p>Vous souhaitez améliorer l’interface utilisateur d’une application déjà existante et cherchez un regard neuf.</p>
+            <p>Je vous propose de résoudre ensemble les problèmes que rencontrent vos utilisateurs.</p>
+          </div>
+        </div>
+        <div class="flex gap-8 w-full">
+          <div class="bg-main h-24  rounded-sm flex">
+            <span class="text-back text-4xl font-bold ml-8 mr-2 mt-2 text-right">3</span>
+          </div>
+          <div class="w-full">
+            <h4 class="text-2xl font-semibold mb-4">Vous avez des besoins ponctuels et souhaitez renforcer votre équipe</h4>
+            <p>Vous chercher quelqu’un pour renforcer votre équipe pendant les périodes de rush, ou pour ajouter des fonctionnalités à votre application.</p>
+            <p>J'interviens sur Grenoble ou Paris, ou en distenciel, de quelques jours à quelques semaines.</p>
+          </div>
+        </div>
+      </div> 
+      <img class="w-48 md:w-96" src="../assets/images/offer.jpg"/> 
+    </div>
+    <div id="booking" class="my-16">
+      <h3 class="font-serif text-4xl font-semibold">Je vous propose de prendre 30 minutes afin d'échanger sur vos projets et vos besoins </h3>
+      <Calendar class="py-16"/>
+      
     </div>
     
   </div>
